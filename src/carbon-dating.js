4fdc461081807e855a62bb8045528ae1e273c5c8
Anastasiya <nastagh96@mail.ru>
2022-04-30 01:40:39 +0200

    complete task carbon-dating and count_cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!sampleActivity||typeof sampleActivity !='string'|| !Number(sampleActivity)||parseFloat(sampleActivity)<0) {
+    return false;
+  } 
+    let number =Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))*HALF_LIFE_PERIOD/0.693);
+    if (number > 0)
+    {
+      return number;
+    }
+    return false;
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(backyard) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let number=0;
+  for (let i=0; i<backyard.length; i++) {
+    for (let j=0; j<backyard[i].length; j++) {
+      if (backyard[i][j]=='^^') {
+        number++;
+      }
+    }
+  }
+  return number;
 }
 
 module.exports = {
