8fa499e0b6531b852396de9e206d7cf798d00b1f
Anastasiya <nastagh96@mail.ru>
2022-05-01 19:54:09 +0200

    complete task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type=true) {
+    this.type=type;
+    this.Dictionary='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let Key = key.toUpperCase();
+    let OutText = '';
+    //j - counter in Key
+    let j=0;
+    let TextToEncrypt=message.toUpperCase();
+    
+    for (let i = 0; i < TextToEncrypt.length; i++) {
+      if (this.Dictionary.indexOf(TextToEncrypt[i]) != -1) {
+          let numberEnc = this.Dictionary.indexOf(TextToEncrypt[i]) + this.Dictionary.indexOf(Key[j]);
+          numberEnc = numberEnc % (this.Dictionary.length);
+          OutText += this.Dictionary[numberEnc];
+          j++;
+          if (j === Key.length) {
+              j = 0;
+          }
+      }
+      else {
+          OutText += TextToEncrypt[i];
+      }
+    }
+    return this.type?OutText:OutText.split('').reverse().join('');
+  }
+
+
+  decrypt(encryptedMessage, key) {
+
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let Key = key.toUpperCase();
+    let OutText='';
+    let j=0;
+    let TextToDecrypt = encryptedMessage.toUpperCase();
+
+    for (let i=0; i< TextToDecrypt.length; i++) {
+      if (this.Dictionary.indexOf(TextToDecrypt[i]) !=-1) {
+        let numberDec = (this.Dictionary.indexOf(TextToDecrypt[i])+this.Dictionary.length-this.Dictionary.indexOf(Key[j]))%this.Dictionary.length;
+        OutText+=this.Dictionary[numberDec];
+        j++;
+        if (j===Key.length) {
+          j=0;
+        }
+      }
+      else {
+        OutText+=TextToDecrypt[i];
+      }
+    }
+    return this.type?OutText:OutText.split('').reverse().join('');
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
+
